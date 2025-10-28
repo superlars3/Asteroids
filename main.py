2f53c18307a96d11d2d9d1456a133110933e158e
superlars3 <larssturkenboom90@gmail.com>
2025-10-28 14:34:56 +0100

    Added more files circleshape player. game runs with white triangle on screen

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
-pygame.init()
 from constants import *
+from player import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}") 
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    gamespeed = pygame.time.Clock()
+    dt = 0
+    Ship = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        Ship.draw(screen)
         pygame.display.flip()
+        dt = gamespeed.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
